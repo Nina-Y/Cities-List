fa2a3b83c77131dfa859ae6fdb1e650c1d06452f
Nina-Y <nina.yushchenko@gmail.com>
2024-07-29 20:23:57 +0300

    Initial commit

---
+++ src/main/java/com/example/citiesList/CitiesListApplication.java
@@ -0,0 +1,13 @@
+package com.example.citiesList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/citiesList/CitiesListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citiesList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
